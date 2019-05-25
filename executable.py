db06cef256506e1b7cb25ac8ece1e06858796c19
Paul Hildebrandt <33395806+paulhldbrndt@users.noreply.github.com>
2019-05-25 17:09:36 +0200

    Update executable.py
    added postprocessing.py

--- executable.py
+++ executable.py
@@ -1,6 +1,11 @@
 import os
+
 print('Random Radio Galaxy Generator')
-amount = int(input('How many radio sources do you want to generate?'))
+amount = int(input('How many radio sources do you wish to generate?'))
 for i in range(amount):
     os.system('python generate.py')
+
+os.system('python postprocessing.py')
+
 print('Done')
+
