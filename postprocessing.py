07d68a928f1dae0399fbb19ae21f8fd7a5691c63
Paul Hildebrandt <33395806+paulhldbrndt@users.noreply.github.com>
2019-05-25 17:13:03 +0200

    Add files via upload
    post processing from fits to image

---
+++ postprocessing.py
@@ -0,0 +1,24 @@
+import os
+from astropy.io import fits
+import matplotlib.pyplot as plt
+
+for filename in os.listdir('fits'):
+    check = filename[:-5] + '.png'
+    if (check not in os.listdir('export')):
+        print(filename)
+        path = 'fits/' + filename
+        image_data = fits.getdata(path, ext=0)
+        plt.grid(b=None)
+        plt.figure(frameon=False)
+        plt.imshow(image_data, cmap='magma')
+
+        # Remove x,y axis labels
+        metadata = plt.gca()
+        metadata.axes.xaxis.set_ticklabels([])
+        metadata.axes.yaxis.set_ticklabels([])
+        # Remove grid
+        metadata.grid(False)
+        metadata.axis('off')
+
+        export_path = 'export/' + filename[:-5] + '.png'
+        plt.savefig(export_path,dpi=250, quality=95)
