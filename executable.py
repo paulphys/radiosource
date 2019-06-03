3af80139d5b590b1c881bb6b2f91be9b188b2c96
Paul Hildebrandt <33395806+paulhldbrndt@users.noreply.github.com>
2019-06-03 20:48:45 +0200

    Update executable.py

--- executable.py
+++ executable.py
@@ -1,11 +1,13 @@
 import os
-
+if not os.path.exists('fits/'):
+    os.makedirs('fits/')
+if not os.path.exists('export/'):
+    os.makedirs('export/')
 print('Random Radio Galaxy Generator')
 amount = int(input('How many radio sources do you wish to generate?'))
 for i in range(amount):
     os.system('python generate.py')
 
+print("Generating Done")
 os.system('python postprocessing.py')
-
-print('Done')
-
+print('Finished')
