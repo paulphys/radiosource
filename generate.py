f618df9c7316a722019bf1ed770d6be8735376b0
Paul Hildebrandt <33395806+paulhldbrndt@users.noreply.github.com>
2019-06-03 20:49:38 +0200

    Update generate.py

--- generate.py
+++ generate.py
@@ -4,9 +4,10 @@ import random
 import uuid
 from astropy.modeling.models import Gaussian2D
 from astropy.nddata import CCDData
+
 y,x = np.mgrid[0:4000, 0:4000]
 data = (Gaussian2D(1,2000,2000,random.randint(50,1000),random.randint(50,1000), theta=random.randint(0,2))(x,y))
-data += 0.01 * np.random.randn(4000,4000)
+data += 0.006 * np.random.randn(4000,4000)
 plt.imshow(data)
 ccd = CCDData(data,meta={'object': 'random radio galaxy', 'filter': 'R'},unit='adu')
 unique_filename = str(uuid.uuid4()) + '.fits'
