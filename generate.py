fa01739eb4b9229de4825bb841b3a44cda1aa4ad
paul <paul.hildebrandt@tum.de>
2021-03-26 17:00:38 +0100

    Update generate.py

--- generate.py
+++ generate.py
@@ -8,6 +8,6 @@ y,x = np.mgrid[0:4000, 0:4000]
 data = (Gaussian2D(1,2000,2000,random.randint(50,1000),random.randint(50,1000), theta=random.randint(0,2))(x,y))
 data += 0.006 * np.random.randn(4000,4000)
 plt.imshow(data)
-ccd = CCDData(data,meta={'object': 'random radio galaxy', 'filter': 'R'},unit='adu')
+ccd = CCDData(data,meta={'object': 'random radio source', 'filter': 'R'},unit='adu')
 unique_filename = str(uuid.uuid4()) + '.fits'
 ccd.write('fits/'+unique_filename)
